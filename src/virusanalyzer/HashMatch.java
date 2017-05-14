7bb647d717f990285780d8604cca8963c669246d
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-05-14 10:35:40 +0530

    initial

---
+++ src/virusanalyzer/HashMatch.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virusanalyzer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Chamath Abeysinghe
+ */
+public class HashMatch {
+    private ArrayList<String> hashList=new ArrayList<String>();
+    
+    HashMatch(){
+        try (
+            BufferedReader br = new BufferedReader(new FileReader(new File("VirusShare_00000.md5.txt")))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+               // process the line.
+               hashList.add(line);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(HashMatch.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String findHash(String path){
+        try {
+            FileInputStream fis = new FileInputStream(new File(path));
+            String md5 = org.apache.commons.codec.digest.DigestUtils.md5Hex(fis);
+            System.out.println(md5);
+            fis.close();
+            return md5;
+            
+        } catch (IOException ex) {
+            Logger.getLogger(HashMatch.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public boolean isMatch(String path){
+        return hashList.contains(findHash(path));
+        
+    }
+}
---
+++ src/virusanalyzer/VirusAnalyzer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virusanalyzer;
+
+/**
+ *
+ * @author Chamath Abeysinghe
+ */
+public class VirusAnalyzer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HashMatch h=new HashMatch();
+        boolean b=h.isMatch("C:\\Projects\\VirusAnalyzer\\build.xml");
+        System.out.println(b);
+    }
+    
+}
